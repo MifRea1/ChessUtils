064d3b46acae7d6b29668adb841702f1ef0f33f0
MifReal <MifReal@list.ru>
2018-07-24 15:11:24 +0400

    Converted variables to constants.

--- players_list_to_csv.php
+++ players_list_to_csv.php
@@ -7,16 +7,16 @@ require_once 'func.php';
 const RATING_LISTS_PATH = 'C:\Chess\Ratings';
 const RATING_LIST_ZIP_FILE_NAME = 'players_list';
 const WHITELIST_FILE_NAME = 'whitelist.txt';
+const RATING_TYPES = ['standard', 'rapid', 'blitz'];
+const RATING_POSITIONS_IN_LINE = ['standard' => 113, 'rapid' => 126, 'blitz' => 139];
 
 loadWhitelistIds(WHITELIST_FILE_NAME);
 $input = getInputFromZip(RATING_LIST_ZIP_FILE_NAME);
 $standardLineLength = strlen(fgets($input));
-$ratingTypes = ['standard', 'rapid', 'blitz'];
-$ratingPositionInLine = ['standard' => 113, 'rapid' => 126, 'blitz' => 139];
-$ratingListFiles = array_combine($ratingTypes, array_map(function($type) {
+$ratingListFiles = array_combine(RATING_TYPES, array_map(function($type) {
     $outputFile = join(DIRECTORY_SEPARATOR, [RATING_LISTS_PATH, $type . '.csv']);
     return fopen($outputFile, 'w');
-}, $ratingTypes));
+}, RATING_TYPES));
 $ratings = [];
 while ($line = fgets($input)) {
     if (isNotWhitelisted($line)) {
@@ -32,8 +32,8 @@ while ($line = fgets($input)) {
     $federation = getFederation($line, true);
     $birthYear = cut($line, 152, 4, true);
     $flags = cut($line, 158, 2);
-    foreach ($ratingTypes as $type) {
-        $ratings[$type] = cut($line, $ratingPositionInLine[$type], 4, true);
+    foreach (RATING_TYPES as $type) {
+        $ratings[$type] = cut($line, RATING_POSITIONS_IN_LINE[$type], 4, true);
         $rating = $ratings[$type] ? $ratings[$type] : $ratings['standard'];
         fwrite($ratingListFiles[$type], join(';', [$id, $name, $title, $federation, $rating, '',  $birthYear, $flags]) . "\n");
     }
